5b001d8385ac725dad9abd1846de8536f282ff43
Anjali Patel <theanjali27@gmail.com>
2025-07-25 02:21:06 -0400

    Add dynamic blog pages and Contentful integration
    Implemented dynamic blog post routing with [slug].astro, fetching blog data from Contentful. Updated advocacy.astro to display articles from Contentful instead of static content, and added new utility functions in contentful.js to fetch blogs and authors. This enables dynamic content management for advocacy and blog pages.

--- haydens-hub/src/lib/contentful.js
+++ haydens-hub/src/lib/contentful.js
@@ -9,3 +9,26 @@ export async function getEntries(contentType) {
   const entries = await client.getEntries({ content_type: contentType });
   return entries.items;
 }
+
+export async function getAllBlogs() {
+  const types = [
+    'academicBlog',
+    'advocacyBlog',
+    'communitySpotlightBlog',
+    'creativeBlog',
+    'youthVoicesBlog',
+  ];
+
+  const all = await Promise.all(
+    types.map((type) =>
+      client.getEntries({ content_type: type, include: 2 }) // include=2 fetches author link
+    )
+  );
+
+  return all.flatMap((res) => res.items);
+}
+
+export async function getAllAuthors() {
+  const authors = await client.getEntries({ content_type: 'author' });
+  return authors.items;
+}
